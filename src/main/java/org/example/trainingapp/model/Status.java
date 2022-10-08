934dae8e14002b330626183961872440a79c1d2e
Bohdan <bohdan.dzoba1@gmail.com>
2022-10-08 19:43:01 +0300

    Create Status.java

--- src/main/java/org/example/trainingapp/model/Status.java
+++ src/main/java/org/example/trainingapp/model/Status.java
@@ -1,8 +1,31 @@
 package org.example.trainingapp.model;
 
+import jakarta.validation.constraints.Min;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
 public class Status {
 
-    enum taskStatus{
+    @Min(value = 1, message = "Target should be at least 1")
+    private int target;
+    private int counter = 0;
+    private TaskStatus taskStatus = TaskStatus.UNSTARTED;
+
+    public Status() {
+        this.target = 0;
+    }
+
+    public Status(int target) {
+        this.target = target;
+    }
+
+    @Override
+    public String toString() {
+        return counter + "/" + target;
+    }
+
+    enum TaskStatus{
         UNSTARTED, INPROGRESS, DONE
     }
 }
